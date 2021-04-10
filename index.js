3f729be1d4540c021d96235e6a48e0b8847966b9
Bruna Franciele <brunafranciele1902@gmail.com>
2021-04-10 15:45:14 -0300

    fix: refactoring endpoints to backend

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const FavoriteRouter = require('./controllers/favoriteController');
-const MarvelRouter = require('./controllers/marvelController');
+const ComicRouter = require('./controllers/comicController');
+const CharacterRouter = require('./controllers/characterController');
 
 require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/favorite', FavoriteRouter);
-app.use('/marvel', MarvelRouter);
+app.use('/comic', ComicRouter);
+app.use('/character', CharacterRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`rodando na porta ${PORT}`));

57f51cc59aa58e7479172fdc76188b39bdbee347
Bruna Franciele <brunafranciele1902@gmail.com>
2021-04-13 17:32:25 -0300

    feat: add connection with remote db

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const config = {
-  host: process.env.HOSTNAME,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: 'Marvel',
+  host:  'us-cdbr-east-03.cleardb.com',
+  user: 'b9f50d46b81772',
+  password: '2e2ae3e5',
+  database: 'heroku_0250d96296c0c45',
 };
 
 const connection = mysql.createPool(config);

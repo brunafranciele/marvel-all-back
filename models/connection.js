cddf442c725cc4e12978cd57f0dab07304132c77
Bruna Franciele <brunafranciele1902@gmail.com>
2021-04-13 17:52:32 -0300

    fix: change config

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const config = {
-  host:  'us-cdbr-east-03.cleardb.com',
+  host: 'us-cdbr-east-03.cleardb.com',
   user: 'b9f50d46b81772',
   password: '2e2ae3e5',
   database: 'heroku_0250d96296c0c45',
